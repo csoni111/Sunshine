9ccd0bf446cd6853f908385e9d19a7d21020c7de
csoni111 <csoni111@gmail.com>
2016-04-03 01:34:20 +0530

    Upgraded to Retrofit library
    Replaced all API Calls with Retrofit and used GSON to parse JSON output.

---
+++ app/src/main/java/com/example/android/sunshine/app/DataModels/WeatherData.java
@@ -0,0 +1,117 @@
+
+package com.example.android.sunshine.app.DataModels;
+
+import java.util.ArrayList;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class WeatherData {
+
+    @SerializedName("city")
+    @Expose
+    private City city;
+    @SerializedName("cod")
+    @Expose
+    private String cod;
+    @SerializedName("message")
+    @Expose
+    private Double message;
+    @SerializedName("cnt")
+    @Expose
+    private Integer cnt;
+    @SerializedName("list")
+    @Expose
+    private java.util.List<com.example.android.sunshine.app.DataModels.list> list = new ArrayList<com.example.android.sunshine.app.DataModels.list>();
+
+    /**
+     * 
+     * @return
+     *     The city
+     */
+    public City getCity() {
+        return city;
+    }
+
+    /**
+     * 
+     * @param city
+     *     The city
+     */
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    /**
+     * 
+     * @return
+     *     The cod
+     */
+    public String getCod() {
+        return cod;
+    }
+
+    /**
+     * 
+     * @param cod
+     *     The cod
+     */
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    /**
+     * 
+     * @return
+     *     The message
+     */
+    public Double getMessage() {
+        return message;
+    }
+
+    /**
+     * 
+     * @param message
+     *     The message
+     */
+    public void setMessage(Double message) {
+        this.message = message;
+    }
+
+    /**
+     * 
+     * @return
+     *     The cnt
+     */
+    public Integer getCnt() {
+        return cnt;
+    }
+
+    /**
+     * 
+     * @param cnt
+     *     The cnt
+     */
+    public void setCnt(Integer cnt) {
+        this.cnt = cnt;
+    }
+
+    /**
+     * 
+     * @return
+     *     The list
+     */
+    public java.util.List<com.example.android.sunshine.app.DataModels.list> getList() {
+        return list;
+    }
+
+    /**
+     * 
+     * @param list
+     *     The list
+     */
+    public void setList(java.util.List<com.example.android.sunshine.app.DataModels.list> list) {
+        this.list = list;
+    }
+
+}
